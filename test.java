e2b4b42524a8a5878dbdbafc40521c89b4c93268
liuchunyu <1026686636@qq.com>
2018-07-26 16:36:38 +0800

    test git

---
+++ test.java
@@ -0,0 +1,39 @@
+public static void readerFile(){
+	FileReader fr = new FileReader("helloWorld.txt");
+	FileWriter fw = new FileWriter("new.txt");
+	int num = fr.read();
+	fr.close();  
+	try{
+
+	}catch(){
+
+	}
+	while((num = fr.read())!=-1){
+		System.Out.Println(); 
+	}
+	BufferedReader
+	BufferedWrited
+	FileWriter fw = new FileWriter("d.txt");
+	BufferedWriter bw = new BufferedWriter(fw);
+	bw.write("Hello World");
+	bw.flush();
+	bw.close();
+	bw = new BufferedWriter(new FileWriter("b.txt"));
+	 readLine();
+	 newLine();
+}
+ public static void testDemo(){
+ 	InputStream is = new FileInputStresam();
+ 	OutputStream os = new FileOutPutStream("b.txt");
+ 	BufferedInputStream bi = new BufferedInputStream();
+ 	long starttime = System.currentTimeMillis();
+ 	public class Student implements Comparable<Student>(){
+ 		 public int compareTo(Student o){
+
+ 		 }
+ 		 Scanner sc = new Scanner(System.in);
+ 		 int count = sc.nextInt();
+ 		 String name = sc.nextLine();
+ 	}
+
+ }
